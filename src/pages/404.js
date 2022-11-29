5fa541db6ddd3097d6bdef024136f8ffe12c8bd5
jpkauri5090 <pbjkmn00@gmail.com>
2022-11-28 21:39:52 -0600

    images files and components

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function NotFound() {
+  return (
+    <Layout>
+      <div>
+        <div>404</div>
+        <p>Sorry, the page doesn't exist</p>
+      </div>
+    </Layout>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,35 @@
-import * as React from "react"
+import { graphql, Link } from "gatsby";
+import * as React from "react";
+import Layout from "../components/Layout";
+import * as styles from "../styles/home.module.css";
+import { GatsbyImage } from "gatsby-plugin-image";
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  console.log(data);
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>Ux designer & web developer based in Winnipeg</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio
+          </Link>
+        </div>
+        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} />
+        {/* <img src="/banner.png" alt="Site logo" style={{ maxWidth: "100%" }} /> */}
+      </section>
+    </Layout>
+  );
 }
+
+export const query = graphql`
+  query MyQuery {
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
+    }
+  }
+`;
