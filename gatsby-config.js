72e50b992695c456a7986e86ad581bd1c5a8d76e
jpkauri5090 <pbjkmn00@gmail.com>
2022-11-28 21:38:53 -0600

    now Portpoilo starts

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,29 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-transformer-remark",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: "J-Playground----",
+    description: "web Portpolio",
+    copyright: "This website is copyright 2021 Jin ",
+    contact: "jin@test.com",
+  },
+};
