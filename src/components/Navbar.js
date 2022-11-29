013be9654b9bad68d11a776cbd0ccd61cb09bc26
jpkauri5090 <pbjkmn00@gmail.com>
2022-11-28 22:38:02 -0600

    css refactoring

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,13 @@
-import { Link, graphql, useStaticQuery } from "gatsby";
+import { Link } from "gatsby";
 import React from "react";
 
 export default function Navbar() {
-  //   const data = useStaticQuery(graphql`
-  //     query Site {
-  //       site {
-  //         siteMetadata {
-  //           title
-  //         }
-  //       }
-  //     }
-  //   `);
-
-  //   const { title } = data.site.siteMetadata;
   return (
     <nav>
-      {/* <h1>{title}</h1> */}
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/projects">Portpolio Projects</Link>
+        <Link to="/projects">Projects</Link>
       </div>
     </nav>
   );
